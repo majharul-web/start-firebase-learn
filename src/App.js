b5196e45976918b81e741a4ceafe3aafcb219201
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-08 11:46:52 +0600

    add google,github,firebase authentication system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider, signOut } from "firebase/auth";
+import { useState } from "react";
+import "./App.css";
+import initializeAuthentication from "./Firebase/firebase.initialize";
 
+initializeAuthentication();
 function App() {
+  const [user, setUser] = useState({});
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+  const facebookProvider = new FacebookAuthProvider();
+  const auth = getAuth();
+
+  // google authentication handler
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedIn = {
+          userName: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(loggedIn);
+      })
+      .then((error) => console.log(error));
+  };
+
+  // github sing in
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider).then((result) => {
+      const { displayName, photoURL } = result.user;
+      const loggedIn = {
+        userName: displayName,
+        photo: photoURL,
+      };
+      setUser(loggedIn);
+    });
+  };
+
+  // facebook sing in
+  const handleFacebookSingIn = () => {
+    signInWithPopup(auth, facebookProvider)
+      .then((result) => {
+        const { displayName, photoURL } = result.user;
+        console.log(result.user);
+        const loggedIn = {
+          userName: displayName,
+          photo: photoURL,
+        };
+        setUser(loggedIn);
+      });
+
+  }
+
+  // logout handler
+  const logoutHandler = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ marginTop: '20px' }}>
+      {!user.userName ? (
+        <div>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+          <button onClick={handleFacebookSingIn}>Facebook Sign In</button>
+        </div>
+      ) : (
+        <button onClick={logoutHandler}>Sign Out</button>
+      )}
+
+      <br />
+
+      {/* show google account data */}
+      {user.userName && (
+        <div>
+          <h1>Welcome {user.userName}</h1>
+          <p>email {user.email}</p>
+          <img src={user.photo} alt='' />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,12 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlJNyDuFFYZ54HMqfzrG2iWRQFMGP3PHE",
+  authDomain: "start-firebase-learn.firebaseapp.com",
+  projectId: "start-firebase-learn",
+  storageBucket: "start-firebase-learn.appspot.com",
+  messagingSenderId: "41963249055",
+  appId: "1:41963249055:web:a6213275762737775ff1aa",
+};
+
+export default firebaseConfig;
